509b16edb70f1a80dd67b5e7d6b210020723c319
meredith.labejof <meredith.labejof@epitech.eu>
2021-07-11 09:25:02 +0200

    [ADD] script to move the player + camera/player

---
+++ Assets/Scripts/Camera_player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_player : MonoBehaviour
+{
+    public GameObject Player;
+    private Vector3 camera_offset;
+
+    void Start()
+    {
+        camera_offset = transform.position - Player.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = Player.transform.position + camera_offset;
+    }
+}
---
+++ Assets/Scripts/player_movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    public GameObject Player;
+    public float speed = 500f;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
+
+    void Update()
+    {
+        float dir_x = Input.GetAxisRaw("Horizontal");
+        float dir_y = Input.GetAxisRaw("Vertical") + 0.5f;
+
+        Player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        rb.velocity = new Vector3(dir_x, rb.velocity.y, dir_y) * speed * Time.fixedDeltaTime; 
+    }
+}
