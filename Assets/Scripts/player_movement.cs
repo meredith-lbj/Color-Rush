bfa3f73524b9a1b84dcb3a12cc0f577061842792
meredith.labejof <meredith.labejof@epitech.eu>
2021-07-15 11:43:14 +0200

    [ADD] code to allow movement in mobile

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -18,6 +18,23 @@ public class player_movement : MonoBehaviour
         float dir_x = Input.GetAxisRaw("Horizontal");
         float dir_y = Input.GetAxisRaw("Vertical");
 
+        if (Input.touchCount > 0) {
+            Touch firstTouch = Input.GetTouch(0);
+
+            if (firstTouch.phase == TouchPhase.Moved) {
+                if (firstTouch.position.x > (Screen.width / 2)) {
+                    dir_x = dir_x + 0.5f;
+                    Player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                    rb.velocity = new Vector3(dir_x, rb.velocity.y, dir_y) * speed * Time.fixedDeltaTime;
+                }
+                else if (firstTouch.position.x < (Screen.width / 2)) {
+                    dir_x = dir_x - 0.5f;
+                    Player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                    rb.velocity = new Vector3(dir_x, rb.velocity.y, dir_y) * speed * Time.fixedDeltaTime;
+                }
+                
+            }
+        }
         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow)) {
             //A modifier
         }
